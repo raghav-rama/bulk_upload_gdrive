54647b2db9020a022e78ec26fac5963290dbf54e
raghav-rama <hackerer528@gmail.com>
2025-08-10 18:17:14 +0530

    feat: add upload command

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
-// use futures::StreamExt;
-// use google_drive3::api::File;
-// use std::sync::Arc;
-// use std::{fs::File as FsFile, path::Path};
-
 use anyhow::{Ok, Result};
 use cli::{Cli, Commands, Parser};
 use drive_client::get_drive_client;
 
-use crate::utils::list_files;
+use crate::utils::{list_files, upload};
 
 mod cli;
 mod drive_client;
@@ -24,83 +19,7 @@ async fn main() -> Result<()> {
             directory,
             folder_id,
             concurrency,
-        } => {
-            println!("Upload, {}, {}, {}", directory, folder_id, concurrency);
-        }
+        } => upload(hub, directory, folder_id, concurrency).await?,
     };
-    // let dir = "/Users/ritz/Downloads/Aubrai papers";
-    // println!("Reading directory: {}", dir);
-    // println!("Directory exists: {}", Path::new(dir).exists());
-
-    // let entries: Vec<_> = std::fs::read_dir(dir)?
-    //     .filter_map(Result::ok)
-    //     .filter(|e| e.path().extension().and_then(|s| s.to_str()) == Some("pdf"))
-    //     .collect();
-
-    // entries
-    //     .iter()
-    //     .for_each(|e| println!("📄 Found PDF: {:?}", e.file_name()));
-    // println!("Total PDF files found: {}", entries.len());
-
-    // if entries.is_empty() {
-    //     println!("No PDF files found in directory");
-    //     return Ok(());
-    // }
-    // let folder_id = "1eAdXLJZZftHewGRn0H6fLGybj5xgF1Yw";
-    // println!("Target Google Drive folder ID: {}", folder_id);
-
-    // futures::stream::iter(entries)
-    //     .map(|entry| {
-    //         let folder_id = folder_id.to_string();
-    //         let hub = Arc::clone(&hub);
-    //         async move {
-    //             let path = entry.path();
-    //             let fname = path.file_name().unwrap().to_string_lossy().to_string();
-    //             println!("Starting upload for: {}", fname);
-    //             let fs_file = match FsFile::open(&path) {
-    //                 Ok(f) => f,
-    //                 Err(e) => {
-    //                     println!("Failed to open file {}: {}", fname, e);
-    //                     return (fname, Err(e.into()));
-    //                 }
-    //             };
-    //             let mime: mime::Mime = "application/pdf".parse().unwrap();
-
-    //             let drive_file = File {
-    //                 name: Some(fname.clone()),
-    //                 parents: Some(vec![folder_id.clone()]),
-    //                 ..Default::default()
-    //             };
-
-    //             let result = hub
-    //                 .files()
-    //                 .create(drive_file)
-    //                 .add_scope(google_drive3::api::Scope::Full)
-    //                 .upload(fs_file, mime.clone())
-    //                 .await;
-
-    //             println!("Upload attempt completed for: {}", fname);
-    //             (fname, result)
-    //         }
-    //     })
-    //     .buffer_unordered(100)
-    //     .for_each(|(fname, result)| async move {
-    //         match result {
-    //             Ok((_, uploaded)) => {
-    //                 println!(
-    //                     "Uploaded '{}' -> ID {}",
-    //                     fname,
-    //                     uploaded.id.unwrap_or_default()
-    //                 );
-    //             }
-    //             Err(e) => {
-    //                 println!("Failed '{}': {}", fname, e);
-    //             }
-    //         }
-    //     })
-    //     .await;
-
-    // let folder_id = "1mQikqFDq82gaRj8mdUYkMYJWk4wc2RYB";
-    // let folder_id = "1p1_tkeh4pRKSTEOz4oKn9apK-McFADLP";
     Ok(())
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,8 @@
 use crate::types::TDriveHub;
-use anyhow::{Ok, Result};
-use google_drive3::api::FileList;
+use anyhow::Result;
+use futures::StreamExt;
+use google_drive3::api::{File, FileList};
+use std::{fs::File as FsFile, path::Path, sync::Arc};
 
 pub async fn list_files(hub: TDriveHub, folder_id: &String) -> Result<()> {
     let result = get_files(hub, folder_id).await?;
@@ -40,4 +42,83 @@ pub async fn get_files(hub: TDriveHub, folder_id: &String) -> Result<FileList> {
         .doit()
         .await?;
     Ok(result.1)
+}
+
+pub async fn upload(
+    hub: TDriveHub,
+    directory: &String,
+    folder_id: &String,
+    concurrency: &usize,
+) -> Result<()> {
+    println!("Reading directory: {}", directory);
+    println!("Directory exists: {}", Path::new(directory).exists());
+
+    let entries: Vec<_> = std::fs::read_dir(directory)?
+        .filter_map(Result::ok)
+        .filter(|e| e.path().extension().and_then(|s| s.to_str()) == Some("pdf"))
+        .collect();
+
+    entries
+        .iter()
+        .for_each(|e| println!("Found PDF: {:?}", e.file_name()));
+    println!("Total PDF files found: {}", entries.len());
+
+    if entries.is_empty() {
+        println!("No PDF files found in directory");
+        return Ok(());
+    }
+    println!("Target Google Drive folder ID: {}", folder_id);
+
+    futures::stream::iter(entries)
+        .map(|entry| {
+            let folder_id = folder_id.to_string();
+            let hub = Arc::clone(&hub);
+            async move {
+                let path = entry.path();
+                let fname = path.file_name().unwrap().to_string_lossy().to_string();
+                println!("Starting upload for: {}", fname);
+                let fs_file = match FsFile::open(&path) {
+                    Ok(f) => f,
+                    Err(e) => {
+                        println!("Failed to open file {}: {}", fname, e);
+                        return (fname, Err(e.into()));
+                    }
+                };
+                let mime: mime::Mime = "application/pdf".parse().unwrap();
+
+                let drive_file = File {
+                    name: Some(fname.clone()),
+                    parents: Some(vec![folder_id.clone()]),
+                    ..Default::default()
+                };
+
+                let result = hub
+                    .files()
+                    .create(drive_file)
+                    .add_scope(google_drive3::api::Scope::Full)
+                    .upload(fs_file, mime.clone())
+                    .await;
+
+                println!("Upload attempt completed for: {}", fname);
+                (fname, result)
+            }
+        })
+        .buffer_unordered(*concurrency)
+        .for_each(|(fname, result)| async move {
+            match result {
+                Ok((_, uploaded)) => {
+                    println!(
+                        "Uploaded '{}' -> ID {}",
+                        fname,
+                        uploaded.id.unwrap_or_default()
+                    );
+                }
+                Err(e) => {
+                    println!("Failed '{}': {}", fname, e);
+                }
+            }
+        })
+        .await;
+
+    Ok(())
 }
